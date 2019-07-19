01d2b9b39607d0bdcf9f8f15de3dcf762520c522
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-19 00:42:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
         resp.write "Your cart is empty"
       end
     elsif req.path.match(/add/)
-      search_term = req.params
+      search_term = req.params["q"]
     else
       resp.write "Path Not Found"
     end
